5c61c388c9b446cf20bcdc51ceb6d0d4d7b81d61
odinakaobi200-crypto <odinakaobi200@gmail.com>
2025-08-21 10:41:45 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~tjFWyCKA#fywqL9E6neaguMHX_9cgjPh9u2smHQISgSH9MwNm1ao",  // add your Session Id here
+  SESSION_ID: "malvin~8rciHbrA#9rMwnc9xUzmTldz4NEohz6tPFay8VrtiIyr5yZnXjkM",  // add your Session Id here
   
   OWNER_NUMBER: "2349134519459", // put your phone number here
   
